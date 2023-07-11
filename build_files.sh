df738c2af3713c9a1d2dde0abd62cdaddf79a899
dipesh singh <singh.dipesh4740@gmail.com>
2023-07-11 17:55:51 +1000

    awdawd

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,16 @@
-echo 'building the project'
+# echo 'building the project'
+# pip install -r requirements.txt
+
+# echo 'make migrations'
+# python manage.py makemigrations
+# python manage.py migrate
+
+# echo 'collect static...'
+# python manage.py collectstatic
+
+# build_files.sh
 pip install -r requirements.txt
 
-echo 'make migrations'
-python manage.py makemigrations
-python manage.py migrate
-
-echo 'collect static...'
-python manage.py collectstatic
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
