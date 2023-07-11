e38e527ded384d90263a76e2c03b35d7a72d9bd9
dipesh singh <singh.dipesh4740@gmail.com>
2023-07-11 17:33:35 +1000

    awdawd

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,9 @@
-pip install -r requirements.txt
-python manage.py collectstatic
+echo 'building the project'
+python 3.9 -m pip install -r requirements.txt
+
+echo 'make migrations'
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+
+echo 'collect static...'
+python3.9 manage.py collectstatic
--- projectyumi/settings.py
+++ projectyumi/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'projectyumi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbyumi', 
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
