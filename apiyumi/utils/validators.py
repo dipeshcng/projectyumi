c7264e3810fe8a3f382c5b9de59568fef2ac1981
unknown <singh.dipesh4740@gmail.com>
2023-09-18 17:55:37 +1000

    pagination:total pages no. added

--- apiyumi/utils/validators.py
+++ apiyumi/utils/validators.py
@@ -29,6 +29,7 @@ class CustomPageNumberPagination(PageNumberPagination):
             'page': self.page.number,
             'page_size': self.page_size,
             'count': self.page.paginator.count,
+            'total_pages' : self.page.paginator.num_pages,
             'next': self.get_next_link(),
             'previous': self.get_previous_link(),
             'results': data,
