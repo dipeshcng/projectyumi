5b25f4c5fa32df004e3e9b7c978c36abcec0b168
unknown <singh.dipesh4740@gmail.com>
2023-09-25 16:25:33 +1000

    status changed for Active for all for now

--- apiyumi/models.py
+++ apiyumi/models.py
@@ -10,7 +10,7 @@ from enum import IntEnum
 class TimeStamp(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=50, choices=STATUS, default='Pending')
+    status = models.CharField(max_length=50, choices=STATUS, default='Active')
 
     class Meta:
         abstract=True
